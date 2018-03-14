9686ade4d684c720fff1a91a0f334614434eb1a9
particleparticle <36958225+particleparticle@users.noreply.github.com>
2018-03-14 19:19:02 +0100

    Update af.js

--- af.js
+++ af.js
@@ -41,3 +41,28 @@ window.onload = function() {
   document.getElementById("berechnennn").onclick = berechne_qqq;
   document.getElementById("berechnennnn").onclick = berechne_tttt;
 }
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
