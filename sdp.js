218f11d66e741df403678bb362b4608645c7b8fe
particleparticle <36958225+particleparticle@users.noreply.github.com>
2018-03-15 20:39:53 +0100

    Update sdp.js

--- sdp.js
+++ sdp.js
@@ -31,14 +31,19 @@ window.onload = function() {
 };
 window.onscroll = function() {myFunction()};
 
+var titl = document.getElementById("myTitl");
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     header.classList.add("sticky");
+    titl.classList.remove("opacityt");
   } else {
     header.classList.remove("sticky");
+    titl.classList.add("opacityt");
+
   }
 }
 function openNav() {
