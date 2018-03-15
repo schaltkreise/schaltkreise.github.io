bf5e3e43fd37bc4c36d6143513e3be6c0a4c8901
particleparticle <36958225+particleparticle@users.noreply.github.com>
2018-03-15 20:39:31 +0100

    Update af.js

--- af.js
+++ af.js
@@ -48,15 +48,19 @@ var sticky = header.offsetTop;
 
 window.onscroll = function() {myFunction()};
 
+var titl = document.getElementById("myTitl");
 var header = document.getElementById("myHeader");
-
 var sticky = header.offsetTop;
+
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     header.classList.add("sticky");
+    titl.classList.remove("opacityt");
   } else {
     header.classList.remove("sticky");
+    titl.classList.add("opacityt");
+
   }
 }
 function openNav() {
